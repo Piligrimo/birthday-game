542449a31e68f25f3b4c5f2dd6a4480c6c2676da
Makin <max4nn@gmail.com>
2026-06-12 19:51:16 +0400

    basic login page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,29 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <h1>Казино Максон</h1>
+  <Login />
 </template>
+
+<style>
+  *{
+    box-sizing: border-box;
+  }
+  body {
+    background-color: #1c6b3e;
+    color: #dbf3e5;
+    
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  input {
+    max-width: 200px;
+  }
+</style>
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,21 @@
+<script setup>
+import {ref} from 'vue'
+const id = ref("")
+</script>
+
+<template>
+  <h3>Логин</h3>
+  <p class="hint">Максон сказал тебе секретный код скопируй его в поле ниже.</p>
+  <p class="hint secondary">(если не сказал, тебя здесь не ждут)</p>
+  <input v-model="id" type="text" placeholder="Секретный код"/>
+  <button> Войти</button>
+</template>
+
+<style>
+.hint {
+  font-size: 11px;
+}
+.hint.secondary {
+  font-size: 8px;
+}
+</style>
